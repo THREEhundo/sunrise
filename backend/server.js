1fc9d31370f59529c5e6c906077b045448b98502
threehund0 <samuelbaik87@gmail.com>
2023-11-16 18:08:32 -0800

    configured mongodb

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ dotenv.config()
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 const port = process.env.PORT || 5000
 import userRoutes from './routes/userRoutes.js'
+import connectDB from './config/db.js'
+
+connectDB()
 
 const app = express()
 
@@ -14,10 +17,3 @@ app.use(notFound)
 app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server is on ${port}`))
-
-// - **POST /api/users** - Register a user
-// - **POST /api/users/auth** - Authenticate a user and get token
-// - **POST /api/users/logout** - Logout user and clear cookie
-// - **POST /api/users/profile** - Get user profile
-// - **POST /api/users/profile** - Update profile
-// - **POST /api/users
