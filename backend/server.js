9a791ad949525e233d4572ef1173a4e0679f8848
threehund0 <samuelbaik87@gmail.com>
2023-11-16 18:46:23 -0800

    parses raw json & forms

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,16 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 8000
 import userRoutes from './routes/userRoutes.js'
 import connectDB from './config/db.js'
 
 connectDB()
 
 const app = express()
+
+app.use(express.json()) // parse raw json
+app.use(express.urlencoded({ extended: true })) // send form data
 
 app.use('/api/users', userRoutes)
 app.get('/', (req, res) => res.send('Server is ready'))
