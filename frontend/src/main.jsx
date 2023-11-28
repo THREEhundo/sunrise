2e3ddf2a953c3570edb2ed41683a71283def987c
threehund0 <samuelbaik87@gmail.com>
2023-11-27 16:29:04 -0800

    Created Login & Register Screen UI's

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 import {
 	createBrowserRouter,
 	createRoutesFromElements,
 	Route,
 	RouterProvider
 } from 'react-router-dom'
-import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
-import HomeScreen from './screens/HomeScreen.jsx'
+import HomeScreen from './screens/HomeScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen.jsx'
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path='/' element={<App />}>
-			<Route index={true} path='/' element={<HomeScreen />} />
+			<Route path='/' element={<HomeScreen />} />
+			<Route path='/login' element={<LoginScreen />} />
+			<Route path='/register' element={<RegisterScreen />} />
 		</Route>
 	)
 )
