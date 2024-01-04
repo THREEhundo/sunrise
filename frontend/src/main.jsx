abad4e71935331e6c93cfb6bbc76c36a8c4b3b92
threehund0 <samuelbaik87@gmail.com>
2024-01-03 17:56:13 -0800

    Private Route created for user when logged out.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,6 +14,8 @@ import './index.css'
 import HomeScreen from './screens/HomeScreen'
 import LoginScreen from './screens/LoginScreen.jsx'
 import RegisterScreen from './screens/RegisterScreen.jsx'
+import ProfileScreen from './screens/ProfileScreen.jsx'
+import PrivateRoutes from './components/PrivateRoutes.jsx'
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
@@ -21,6 +23,10 @@ const router = createBrowserRouter(
 			<Route index={true} path='/' element={<HomeScreen />} />
 			<Route path='/login' element={<LoginScreen />} />
 			<Route path='/register' element={<RegisterScreen />} />
+			{/* Private Routes */}
+			<Route path='' element={<PrivateRoutes />}>
+				<Route path='/profile' element={<ProfileScreen />} />
+			</Route>
 		</Route>
 	)
 )
