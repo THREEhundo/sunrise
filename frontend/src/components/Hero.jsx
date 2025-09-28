45abede47eb7744b01cbfa21392a6ef31001742b
threehund0 <samuelbaik87@gmail.com>
2025-09-27 18:07:10 -0700

    Edit shown elements depending on login status

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,7 +1,10 @@
 import { Container, Card, Button } from 'react-bootstrap'
+import { useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
 
 const Hero = () => {
+	const { userInfo } = useSelector(state => state.auth)
+
 	return (
 		<div className=' py-5'>
 			<Container className='d-flex justify-content-center'>
@@ -13,14 +16,28 @@ const Hero = () => {
 						from jet lag.
 					</p>
 					<div className='d-flex'>
-						<LinkContainer to='/login'>
-							<Button variant='primary' className='me-3'>
-								Sign In
-							</Button>
-						</LinkContainer>
-						<LinkContainer to='/register'>
-							<Button variant='secondary'>Register</Button>
-						</LinkContainer>
+						{userInfo ? (
+							<>
+								<LinkContainer to='/sleepform'>
+									<Button variant='primary'>
+										Get Started
+									</Button>
+								</LinkContainer>
+							</>
+						) : (
+							<>
+								<LinkContainer to='/login'>
+									<Button variant='primary' className='me-3'>
+										Sign In
+									</Button>
+								</LinkContainer>
+								<LinkContainer to='/register'>
+									<Button variant='secondary'>
+										Register
+									</Button>
+								</LinkContainer>
+							</>
+						)}
 					</div>
 				</Card>
 			</Container>
